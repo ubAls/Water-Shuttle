30115dfa8a85711fcdd18c15b17ccfa353b30563
ubAls <97922570+ubAls@users.noreply.github.com>
2023-05-08 15:21:40 +0900

    Create p5_ai.js

---
+++ p5_ai.js
@@ -0,0 +1,49 @@
+const modelURL = 'https://teachablemachine.withgoogle.com/models/_______/';
+const serialPort = 'COM_';
+
+let classifier;
+let serial;
+let video;
+let flippedVideo;
+let label;
+
+function preload() {
+    classifier = ml5.imageClassifier(modelURL + 'model.json');
+    serial = new p5.SerialPort();
+}
+
+function setup() {
+    serial.open(serialPort);
+    createCanvas(320, 260);
+    video = createCapture(VIDEO);
+    video.size(320, 240);
+    video.hide();
+    flippedVideo = ml5.flipImage(video);
+    classifyVideo();
+}
+
+function draw() {
+    background(0);
+    image(flippedVideo, 0, 0);
+    fill(255);
+    textSize(16);
+    textAlign(CENTER);
+    text(label, width / 2, height - 4);
+}
+
+function classifyVideo() {
+    flippedVideo = ml5.flipImage(video)
+    classifier.classify(flippedVideo, gotResult);
+    flippedVideo.remove();
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.error(error);
+        return;
+    }
+    label = String(results[0].label);
+    console.log(label);
+    serial.write(label);
+    classifyVideo();
+}
